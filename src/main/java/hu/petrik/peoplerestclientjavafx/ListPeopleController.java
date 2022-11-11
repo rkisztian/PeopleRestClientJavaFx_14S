fbe27dff6612a9775a7f741aca66e351060e68ee
Budaházi Bence <budahazi.bence@petrik.hu>
2022-11-11 17:25:34 +0100

    Emberek listázása

--- src/main/java/hu/petrik/peoplerestclientjavafx/ListPeopleController.java
+++ src/main/java/hu/petrik/peoplerestclientjavafx/ListPeopleController.java
@@ -1,11 +1,14 @@
 package hu.petrik.peoplerestclientjavafx;
 
+import com.google.gson.Gson;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.io.IOException;
+import java.util.Collections;
 
 public class ListPeopleController {
 
@@ -25,6 +28,33 @@ public class ListPeopleController {
     private TableColumn<Person, String> emailCol;
     @FXML
     private TableColumn<Person, Integer> ageCol;
+
+    @FXML
+    private void initialize() {
+        idCol.setCellValueFactory(new PropertyValueFactory<>("id")); //getId() függvény eredményét jeleníti meg
+        nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        emailCol.setCellValueFactory(new PropertyValueFactory<>("email"));
+        ageCol.setCellValueFactory(new PropertyValueFactory<>("age"));
+        Platform.runLater(() -> {
+            try {
+                Response response = RequestHandler.get(App.BASE_URL);
+                String content = response.getContent();
+                Gson converter = new Gson();
+                Person[] people = converter.fromJson(content, Person[].class);
+                //Collections.addAll(peopleTable.getItems(), people);
+                for (Person person: people) {
+                    peopleTable.getItems().add(person);
+                }
+            } catch (IOException e) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Hiba");
+                alert.setHeaderText("Hiba történt az adatok lekérése során");
+                alert.setContentText(e.getMessage());
+                alert.showAndWait();
+                Platform.exit();
+            }
+        });
+    }
 
     @FXML
     public void insertClick(ActionEvent actionEvent) {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module hu.petrik.peoplerestclientjavafx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.google.gson;
 
-
-    opens hu.petrik.peoplerestclientjavafx to javafx.fxml;
+    opens hu.petrik.peoplerestclientjavafx to javafx.fxml, com.google.gson;
     exports hu.petrik.peoplerestclientjavafx;
 }
